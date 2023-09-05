79d824d47382dfa8dc9bde81441e0fee20e52d9e
guanshan <1608198396@qq.com>
2023-09-05 16:59:41 +0800

    add patch-package

--- src/PDF/index.tsx
+++ src/PDF/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-// import Pdf from 'react-native-pdf'
+import Pdf from 'react-native-pdf'
 
 function PDFComponent() {
   return (
     <View>
-          {/* <Pdf
-              trustAllCerts={false}
+      <Pdf
+        trustAllCerts={false}
         source={{
           uri: 'https://www.amcfortune.com/article/20230531/CN_50030000_007937_FA010080_20230001_007937_20230531_090000_01.pdf',
           cache: true,
@@ -23,8 +23,8 @@ function PDFComponent() {
         onPressLink={uri => {
           console.log(`Link pressed: ${uri}`)
         }}
-        style={{width: '100%', height: 500}}
-      /> */}
+        style={{ width: '100%', height: 500 }}
+      />
       <Text>123123</Text>
     </View>
   )
